1a1ebe3748b24a1e8331003b35213df00db613e7
Chris Smothers <chris.smothers@gmail.com>
2013-01-26 12:11:21 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Positivespace::Application.config.secret_token = 'd422d1cf5e908dce776fda36214d2b35f72fa2b7ebe240fd9472123c5cd932eb5b50f8ed2e2814a07d97bd072740eafd40d1f096b3213d18b6490fe90f633fba'
