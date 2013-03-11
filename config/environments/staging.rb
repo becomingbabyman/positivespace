e589016f3a184ed989a7b16b11b5d59a9722d839
Chris Smothers <chris.smothers@gmail.com>
2013-03-11 10:24:15 -0400

    fix env configs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-Objay::Application.configure do
+Positivespace::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
