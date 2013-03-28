46569986686bd8c3775e011279a5c5364305cd1b
Chris Smothers <chris.smothers@gmail.com>
2013-03-27 17:16:41 -0700

    bust the cache
    come back and find a better solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
 
 	before_filter :intercept_html_requests
 
+	# TODO: REMOVE: PERFORMANCE: This is killer, but I don't know a better way to bust the cache when files are updated. Maybe if we can get timestamped html served from a CDN we can remove this
+	before_filter :set_cache_buster
+
 	rescue_from CanCan::AccessDenied do |exception|
 		if request.format == Mime::HTML
 			redirect_to root_url, :alert => "You do not have permission to view that page."
@@ -45,5 +48,11 @@ private
 		!protect_against_forgery? || request.get? ||
 			form_authenticity_token == params[request_forgery_protection_token] || form_authenticity_token == request.headers['X-CSRF-Token'] ||
 			request.headers['X-XSRF-Token'] && form_authenticity_token == request.headers['X-XSRF-Token'].gsub!(/\A"|"\Z/, '')
-		end
+	end
+
+	def set_cache_buster
+		response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+		response.headers["Pragma"] = "no-cache"
+		response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+	end
 end
