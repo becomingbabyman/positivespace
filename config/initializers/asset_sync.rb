a2e6108812d813757a0b0cb4c05c8abbcd6dbc2f
Chris Smothers <chris.smothers@gmail.com>
2013-05-19 11:14:33 -0400

    enable gzip?

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -11,11 +11,11 @@ AssetSync.configure do |config|
   # config.existing_remote_files = "keep"
   #
   # Automatically replace files with their equivalent gzip compressed version
-  # config.gzip_compression = true
+  config.gzip_compression = true
   #
   # Use the Rails generated 'manifest.yml' file to produce the list of files to
   # upload instead of searching the assets directory.
-  # config.manifest = true
+  config.manifest = true
   #
   # Fail silently.  Useful for environments such as Heroku
   # config.fail_silently = true
