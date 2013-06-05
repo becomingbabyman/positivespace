9c4b2f35abccb6b7b6fed3046282709714a02b37
Chris Smothers <chris.smothers@gmail.com>
2013-06-05 17:23:06 -0400

    update purpose

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,5 @@
+Sidekiq.configure_server do |config|
+	# TODO: increase when you have more redis memory to spare
+	# Keep it low to keep the memory footprint low
+	config.failures_max_count = 20
+end
