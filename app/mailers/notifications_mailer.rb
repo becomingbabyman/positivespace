bf794181adfa100121cdc56962ebd8a42d422fda
Chris Smothers <chris.smothers@gmail.com>
2013-06-11 08:31:27 -0400

    improve html email style

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -18,16 +18,15 @@ class NotificationsMailer < ActionMailer::Base
     from.display_name = @message.from.name
     to = Mail::Address.new @message.to.email
     to.display_name = @message.to.name
-    mail to: to.format, from: from.format, subject: "Reply to #{@message.from.name}"
+    mail to: to.format, from: from.format, subject: "Reply to #{@message.from.name} on Positive Space"
   end
 
   def daily_new_messages_digest user_id
     @user = User.find user_id
     @messages = @user.recieved_messages.order("created_at")
-    # @reply_path = "conversations/#{@message.conversation_id}?message_id=#{@message.id}"
 
     to = Mail::Address.new @user.email
     to.display_name = @user.name
-    mail to: to.format, subject: "Today's new messages"
+    mail to: to.format, subject: "Today's new Positive Space messages"
   end
 end
