5594ad7fe4d86c81bb4408936bd8290795b825c5
Chris Smothers <chris.smothers@gmail.com>
2013-07-20 01:56:10 -0400

    tmp

--- db/migrate/20130720012142_create_magnetisms_and_achievements.rb
+++ db/migrate/20130720012142_create_magnetisms_and_achievements.rb
@@ -19,64 +19,64 @@ class CreateMagnetismsAndAchievements < ActiveRecord::Migration
     add_column :users, :magnetisms_count, :integer, default: 0
 
 
-    create_table :achievements do |t|
-      t.string :name
-      t.text :description
+    # create_table :achievements do |t|
+    #   t.string :name
+    #   t.text :description
 
-      t.timestamps
-    end
-    add_index :achievements, :name
+    #   t.timestamps
+    # end
+    # add_index :achievements, :name
 
-    rename_column :users, :achievements, :achievements_list
-    change_column_default :users, :achievements_list, []
-    add_column :users, :achievements_count, :integer, default: 0
-
-
-    create_table :wins do |t|
-      t.integer :achievement_id
-      t.integer :user_id
-
-      t.timestamps
-    end
-    add_index :wins, :achievement_id
-    add_index :wins, :user_id
-    add_index :wins, [:achievement_id, :user_id]
+    # rename_column :users, :achievements, :achievements_list
+    # change_column_default :users, :achievements_list, []
+    # add_column :users, :achievements_count, :integer, default: 0
 
 
-    Win.reset_column_information
-    Achievement.reset_column_information
-    Magnetism.reset_column_information
-    User.reset_column_information
+    # create_table :wins do |t|
+    #   t.integer :achievement_id
+    #   t.integer :user_id
 
-    say_with_time "Make user magnetisms" do
-      User.find_in_batches do |group|
-        sleep(10) # Make sure it doesn't get too crowded in there!
-        group.each do |u|
-          u.magnetisms.create(inc: u.magnetism, reason: 'onboarding complete', attachable: u.conversations.first, callback: :none) if u.magnetism > 0
-        end
-      end
-    end
+    #   t.timestamps
+    # end
+    # add_index :wins, :achievement_id
+    # add_index :wins, :user_id
+    # add_index :wins, [:achievement_id, :user_id]
 
-    say_with_time "Make achievements_list an array" do
-      User.find_in_batches do |group|
-        sleep(10) # Make sure it doesn't get too crowded in there!
-        group.each do |u|
-          u.achievements_list = u.achievements_list.map{|k,v| k.to_s}
-          u.save
-        end
-      end
-    end
 
-    say_with_time "Make achievements" do
-      User.find_in_batches do |group|
-        sleep(10) # Make sure it doesn't get too crowded in there!
-        group.each do |u|
-          u.achievements_list.each do |name|
-            u.track_achievement name
-          end
-        end
-      end
-    end
+    # Win.reset_column_information
+    # Achievement.reset_column_information
+    # Magnetism.reset_column_information
+    # User.reset_column_information
+
+    # say_with_time "Make user magnetisms" do
+    #   User.find_in_batches do |group|
+    #     sleep(10) # Make sure it doesn't get too crowded in there!
+    #     group.each do |u|
+    #       u.magnetisms.create(inc: u.magnetism, reason: 'onboarding complete', attachable: u.conversations.first, callback: :none) if u.magnetism > 0
+    #     end
+    #   end
+    # end
+
+    # say_with_time "Make achievements_list an array" do
+    #   User.find_in_batches do |group|
+    #     sleep(10) # Make sure it doesn't get too crowded in there!
+    #     group.each do |u|
+    #       u.achievements_list = u.achievements_list.map{|k,v| k.to_s}
+    #       u.save
+    #     end
+    #   end
+    # end
+
+    # say_with_time "Make achievements" do
+    #   User.find_in_batches do |group|
+    #     sleep(10) # Make sure it doesn't get too crowded in there!
+    #     group.each do |u|
+    #       u.achievements_list.each do |name|
+    #         u.track_achievement name
+    #       end
+    #     end
+    #   end
+    # end
 
   end
 end
