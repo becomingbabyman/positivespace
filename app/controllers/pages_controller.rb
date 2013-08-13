d0aaed5e60a9ab2e44e3c0a62b9886e425925a4e
Chris Smothers <chris.smothers@gmail.com>
2013-08-13 01:30:34 -0400

    send bots to seo server

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class PagesController < ApplicationController
 
 	def home
@@ -14,6 +16,13 @@ class PagesController < ApplicationController
 
 		if request.headers['X-PJAX']
 			render :layout => false, :template => 'pages/wildcard'
+		elsif request.env["HTTP_USER_AGENT"] and request.env["HTTP_USER_AGENT"].match(/\(.*https?:\/\/.*\)/)
+			uri = URI("http://seo.positivespace.io#{request.path}")
+			uri.query = URI.encode_www_form(params)
+			req = Net::HTTP::Get.new(uri)
+			req["x-forwarded-host"] = "www.positivespace.io"
+			res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+			render text: res.body, layout: false, :content_type => "text/html"
 		else
 			render :layout => 'angular', :template => 'pages/wildcard'
 		end
