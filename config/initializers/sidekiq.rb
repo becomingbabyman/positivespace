fb36cce659f676ae99468e7c260cd3085b9df82f
Chris Smothers <chris.smothers@gmail.com>
2013-08-29 17:48:53 -0400

    fixes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,7 @@
-Sidekiq.configure_server do |config|
-	# TODO: increase when you have more redis memory to spare
-	# Keep it low to keep the memory footprint low
-	config.failures_max_count = 20
+if defined?(Sidekiq)
+	Sidekiq.configure_server do |config|
+		# TODO: increase when you have more redis memory to spare
+		# Keep it low to keep the memory footprint low
+		config.failures_max_count = 20
+	end
 end
